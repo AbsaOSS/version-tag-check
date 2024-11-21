35bf5aa3ea7a07862b842a3998f97a8821b4a7d2
miroslavpojer <miroslav.pojer@absa.africa>
2024-11-21 13:47:18 +0100

    - Improved unit test coverage.

--- tests/test_version_tag_check_action.py
+++ tests/test_version_tag_check_action.py
@@ -58,14 +58,27 @@ def test_validate_inputs_missing_variables(monkeypatch, caplog, missing_var, err
 
 # run
 
-def test_run_successful(mocker, tmp_path):
+@pytest.mark.parametrize(
+    "version_tag, existing_tags",
+    [
+        ("v0.0.1", []),                         # New version with no existing tags
+        ("v0.1.0", []),                         # New version with no existing tags
+        ("v1.0.0", []),                         # New version with no existing tags
+        ("v0.0.2", ["v0.0.1"]),                 # Patch increment
+        ("v1.2.0", ["v1.1.0"]),                 # Minor increment
+        ("v2.0.0", ["v1.9.9"]),                 # Major increment
+        ("v2.1.0", ["v2.0.5", "v2.0.0"]),       # New Release serie
+        ("v3.0.1", ["v2.9.9", "v1.0.0"]),       # Increment with Gaps
+        ("v1.5.2", ["v2.0.0", "v1.5.1"]),       # Backport increment
+    ],
+)
+def test_run_successful(mocker, version_tag, existing_tags):
     # Set environment variables
     env_vars = {
         "INPUT_GITHUB_TOKEN": "fake_token",
-        "INPUT_VERSION_TAG": "v1.0.1",
+        "INPUT_VERSION_TAG": version_tag,
         "INPUT_GITHUB_REPOSITORY": "owner/repo",
     }
-    # Update os.environ with the test environment variables
     os.environ.update(env_vars)
 
     # Mock sys.exit to prevent the test from exiting
@@ -79,7 +92,7 @@ def test_run_successful(mocker, tmp_path):
     # Mock the GitHubRepository class
     mock_repository_class = mocker.patch("version_tag_check.version_tag_check_action.GitHubRepository")
     mock_repository_instance = mock_repository_class.return_value
-    mock_repository_instance.get_all_tags.return_value = []
+    mock_repository_instance.get_all_tags.return_value = existing_tags
 
     # Mock the NewVersionValidator class
     mock_validator_class = mocker.patch("version_tag_check.version_tag_check_action.NewVersionValidator")
@@ -92,25 +105,47 @@ def test_run_successful(mocker, tmp_path):
 
     mock_exit.assert_called_once_with(0)
 
-def test_run_invalid_version_format(mocker, tmp_path, caplog):
+
+@pytest.mark.parametrize(
+    "version_tag, existing_tags, is_valid_format, is_valid_increment, expected_exit_code, error_message",
+    [
+        ("invalid_version", [], False, True, 1, "Tag does not match the required format"),  # Invalid format
+        ("invalid_version", ["v1.0.0"], False, True, 1, "Tag does not match the required format"),  # Invalid format
+        ("v1.0.3", ["v1.0.1"], True, False, 1, "New tag is not valid."),                    # Invalid increment
+        ("v1.0.0", ["v1.0.0"], True, False, 1, "New tag is not valid."),                    # Existing tag
+        ("v1.4.1", ["v2.0.0", "v1.4.2"], True, False, 1, "New tag is not valid."),          # Invalid backport increment
+        ("1.0.0", [], False, True, 1, "Tag does not match the required format"),          # Invalid format and increment
+    ],
+)
+def test_run_unsuccessful(mocker, caplog, version_tag, existing_tags, is_valid_format, is_valid_increment, expected_exit_code, error_message):
     # Set environment variables
     env_vars = {
         "INPUT_GITHUB_TOKEN": "fake_token",
-        "INPUT_VERSION_TAG": "invalid_version",
+        "INPUT_VERSION_TAG": version_tag,
         "INPUT_GITHUB_REPOSITORY": "owner/repo",
     }
     os.environ.update(env_vars)
 
-    # Mock sys.exit
+    # Mock sys.exit to raise a SystemExit for assertion
     def mock_exit(code):
         raise SystemExit(code)
 
     mocker.patch("sys.exit", mock_exit)
 
-    # Mock the Version class used in VersionTagCheckAction
+    # Mock the Version class
     mock_version_class = mocker.patch("version_tag_check.version_tag_check_action.Version")
     mock_version_instance = mock_version_class.return_value
-    mock_version_instance.is_valid_format.return_value = False  # Simulate invalid format
+    mock_version_instance.is_valid_format.return_value = is_valid_format
+
+    # Mock the GitHubRepository class
+    mock_repository_class = mocker.patch("version_tag_check.version_tag_check_action.GitHubRepository")
+    mock_repository_instance = mock_repository_class.return_value
+    mock_repository_instance.get_all_tags.return_value = existing_tags
+
+    # Mock the NewVersionValidator class
+    mock_validator_class = mocker.patch("version_tag_check.version_tag_check_action.NewVersionValidator")
+    mock_validator_instance = mock_validator_class.return_value
+    mock_validator_instance.is_valid_increment.return_value = is_valid_increment
 
     # Run the action
     caplog.set_level(logging.ERROR)
@@ -118,40 +153,8 @@ def test_run_invalid_version_format(mocker, tmp_path, caplog):
     with pytest.raises(SystemExit) as e:
         action.run()
 
-    # Assert that sys.exit was called with exit code 1
-    assert e.value.code == 1
+    # Assert sys.exit was called with the correct code
+    assert e.value.code == expected_exit_code
 
-    assert 'Tag does not match the required format' in caplog.text
-
-def test_run_invalid_version_increment(mocker, tmp_path):
-    # Set environment variables
-    env_vars = {
-        "INPUT_GITHUB_TOKEN": "fake_token",
-        "INPUT_VERSION_TAG": "v1.0.2",
-        "INPUT_GITHUB_REPOSITORY": "owner/repo",
-    }
-    os.environ.update(env_vars)
-
-    # Mock sys.exit
-    mock_exit = mocker.patch("sys.exit")
-
-    # Mock the Version class
-    mock_version_class = mocker.patch("version_tag_check.version_tag_check_action.Version")
-    mock_version_instance = mock_version_class.return_value
-    mock_version_instance.is_valid_format.return_value = True
-
-    # Mock the GitHubRepository class
-    mock_repository_class = mocker.patch("version_tag_check.version_tag_check_action.GitHubRepository")
-    mock_repository_instance = mock_repository_class.return_value
-    mock_repository_instance.get_all_tags.return_value = []  # Simulate existing versions if needed
-
-    # Mock the NewVersionValidator class to return False for is_valid_increment
-    mock_validator_class = mocker.patch("version_tag_check.version_tag_check_action.NewVersionValidator")
-    mock_validator_instance = mock_validator_class.return_value
-    mock_validator_instance.is_valid_increment.return_value = False
-
-    # Run the action
-    action = VersionTagCheckAction()
-    action.run()
-
-    mock_exit.assert_called_once_with(1)
+    # Assert error message in logs
+    assert error_message in caplog.text
